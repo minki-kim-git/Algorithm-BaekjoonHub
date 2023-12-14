180cc65a35e123e38884470a0fd478e0ddbc0003
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2023-12-15 05:08:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -0,0 +1,11 @@
+func solution(_ n:Int) -> String {
+    var ans = ""
+    for i in 0..<n{
+        if i % 2 == 0 {
+            ans += "수"
+        } else {
+            ans += "박"
+        }
+    }
+    return ans
+}
