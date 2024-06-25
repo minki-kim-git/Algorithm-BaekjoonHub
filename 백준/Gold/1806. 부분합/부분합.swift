3ecff2ef801667f69b6296c8962795ca17e65399
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-06-25 17:06:41 +0900

    [Gold IV] Title: 부분합, Time: 24 ms, Memory: 83076 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+let n = io.readInt()
+let s = io.readInt()
+
+var arr = [Int]()
+for _ in 0..<n {
+    arr.append(io.readInt())
+}
+
+var ans = 1000000
+var st = 0
+var en = -1
+var count = 0
+var sum = 0
+
+while st < n {
+    if sum >= s {
+        ans = min(count,ans)
+        if count == 1 { break }
+        sum -= arr[st]
+        st += 1
+        count -= 1
+    } else if sum < s {
+        en += 1
+        if en == n { break }
+        sum += arr[en]
+        count += 1
+    } 
+}
+
+ans == 1000000 ? print(0) : print(ans)
