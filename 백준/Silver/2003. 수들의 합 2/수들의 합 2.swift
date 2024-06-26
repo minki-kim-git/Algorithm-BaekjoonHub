2e075671c99dcb5fd568ad4ae4bc5ecce14b8172
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-06-26 19:11:06 +0900

    [Silver IV] Title: 수들의 합 2, Time: 8 ms, Memory: 70012 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.swift
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.swift
@@ -1,13 +1,10 @@
 let n = readLine()!.split(separator: " ").map{Int(String($0))!}
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-var sum = 0
+var sum = input[0]
 var st = 0
 var en = 0
 var ans = 0
 while st < n[0] {
-    if st == en {
-        sum = input[st]
-    }
     if sum < n[1] {
         en += 1
         if en == n[0] { break }
