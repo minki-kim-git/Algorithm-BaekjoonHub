0d17abec0c88a218a5f31c0ee51ddfa77068db27
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-07-04 08:56:47 +0900

    [Silver III] Title: 합 구하기, Time: 120 ms, Memory: 85644 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+let n = io.readInt()
+var arr = [0]
+var sum = [0]
+
+for _ in 0..<n {
+    let input = io.readInt()
+    arr.append(input)
+    sum.append(input+(sum.last ?? 0))
+}
+
+let m = io.readInt()
+for _ in 0..<m {
+    let i = io.readInt()
+    let j = io.readInt()
+    print(sum[j] - sum[i-1])
+}
