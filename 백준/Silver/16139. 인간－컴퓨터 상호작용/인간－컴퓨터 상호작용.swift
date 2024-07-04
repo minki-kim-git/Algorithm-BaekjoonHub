2f01cc2ee06e087839849e7cd6250bfb67c207bf
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-07-04 10:27:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 788 ms, Memory: 400420 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+let s = io.readString()
+var dic = [["":0]]
+let q = io.readInt()
+
+for i in s.enumerated() {
+    var a = dic.last!
+    if let val = a[String(i.element)] {
+        a[String(i.element)] = a[String(i.element)]! + 1
+    } else {
+        a[String(i.element)] = 1
+    }
+    dic.append(a)
+}
+
+for _ in 0..<q {
+    let a = io.readString()
+    let l = io.readInt() + 1
+    let r = io.readInt() + 1
+    let sum = (dic[r][a] ?? 0) - (dic[l-1][a] ?? 0 )
+    print(sum)
+}
