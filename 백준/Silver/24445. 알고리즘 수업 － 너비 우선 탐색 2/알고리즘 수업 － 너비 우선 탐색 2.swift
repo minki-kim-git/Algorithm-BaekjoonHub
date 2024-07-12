f4894324a332c1185299b6b8b7b13e6458b02f21
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-07-12 20:55:49 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 152 ms, Memory: 92772 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let n = io.readInt()
+let m = io.readInt()
+let r = io.readInt()
+
+var vis = Array(repeating: 0, count: n+1)
+var arr = Array(repeating: Array(repeating: 0, count: 0), count: n+1)
+
+for _ in 0..<m {
+    let u = io.readInt()
+    let v = io.readInt()
+    arr[u].append(v)
+    arr[v].append(u)
+}
+
+var queue = [r]
+vis[r] = 1
+var index = 0
+
+while queue.count > index {
+    for i in arr[queue[index]].sorted(by:>) {
+        if vis[i] != 0 { continue }
+        queue.append(i)
+        vis[i] = queue.count
+    }
+    index += 1
+}
+
+for i in 1...n {
+    print(vis[i])
+}
