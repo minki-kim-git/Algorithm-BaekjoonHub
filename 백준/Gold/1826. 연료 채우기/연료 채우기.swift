22f98ba76dacfd8dcc4dd80285e478af54d7c598
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-07-25 18:04:26 +0900

    [Gold II] Title: 연료 채우기, Time: 64 ms, Memory: 80380 KB -BaekjoonHub

---
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.swift
@@ -0,0 +1,128 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+let n = io.readInt()
+
+var ans = 0
+var heap = [0]
+var dic = [Int:[Int]]()
+
+for _ in 0..<n {
+    let a = io.readInt()
+    let b = io.readInt()
+    if dic[a] == nil {
+        dic[a] = [b]
+    } else {
+        dic[a]!.append(b)
+    }
+}
+
+let l = io.readInt()
+var p = io.readInt()
+
+func push(_ num: Int) {
+    heap.append(num)
+    var i = heap.count-1
+    while i > 1 {
+        let p = i / 2
+        if heap[p] > heap[i] { break }
+        heap.swapAt(p, i)
+        i = p
+    }
+}
+
+func pop() -> Int{
+    ans += 1
+    heap.swapAt(1, heap.count-1)
+    let maxp = heap.popLast()!
+    var i = 1
+    while i*2 < heap.count {
+        let lc = i * 2
+        let rc = i * 2 + 1
+        var maxc = lc
+        if rc < heap.count && heap[lc] < heap[rc] {
+            maxc = rc
+        }
+        if heap[maxc] <= heap[i] {
+            break
+        }
+        heap.swapAt(maxc, i)
+        i = maxc
+    }
+    return maxp
+}
+
+func ansNum() -> Int {
+    for i in 1...l {
+        if let val = dic[i] {
+            _ = val.map {
+                push($0)
+            }
+        }
+        p -= 1
+        if p < 0 {
+            return -1
+        } else if p == 0 && i != l {
+            if heap.count > 1 {
+                 p += pop()
+            }
+        }
+    }
+    return ans
+}
+
+print(ansNum())
