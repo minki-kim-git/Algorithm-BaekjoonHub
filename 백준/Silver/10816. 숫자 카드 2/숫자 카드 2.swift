b9cb2bb9e0c8bd87a8124574a625a6a208d31f2c
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-07-26 18:44:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 376 ms, Memory: 102464 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
@@ -1,20 +1,105 @@
-var dic = [String:Int]()
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+let n = io.readInt()
+
+var arr = [Int]()
 var ans = ""
-let _ = Int(readLine()!)!
-let _ = readLine()!.split(separator: " ").map {
-    if let val = dic[String($0)] {
-        dic[String($0)] = val + 1
-    } else {
-        dic[String($0)] = 1
-    }
+
+for _ in 0..<n {
+    arr.append(io.readInt())
 }
-let _ = Int(readLine()!)!
-let _ = readLine()!.split(separator: " ").map {
-    if dic[String($0)] == nil {
-        ans += "0 "
-    } else {
-        ans += "\(dic[String($0)]!) "
+
+arr = arr.sorted()
+
+let m = io.readInt()
+
+func lowIndex(_ num:Int) -> Int{
+    var st = 0
+    var en = arr.count
+    while st < en {
+        let mid = (st + en)/2
+        if arr[mid] >= num {
+            en = mid
+        } else {
+            st = mid + 1
+        }
     }
+    return st
 }
+
+func upIndex(_ num:Int) -> Int {
+    var st = 0
+    var en = arr.count
+    while st < en {
+        let mid = (st + en)/2
+        if arr[mid] > num {
+            en = mid
+        } else {
+            st = mid + 1
+        }
+    }
+    return en
+}
+
+for _ in 0..<m {
+    let num = io.readInt()
+    ans += "\(upIndex(num) - lowIndex(num)) "
+}
+
 ans.removeLast()
 print(ans)
