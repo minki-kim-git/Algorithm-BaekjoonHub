89f8777fac2a1fc91f3e384c6c366d20ab2d2a33
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-08-02 17:01:24 +0900

    [Silver II] Title: 랜선 자르기, Time: 24 ms, Memory: 79832 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let k = io.readInt()
+let n = io.readInt()
+
+var arr = [Int]()
+
+for _ in 0..<k {
+    arr.append(io.readInt())
+}
+
+func kcount(_ len:Int) -> Bool {
+    var count = 0
+    for i in arr {
+       count += i / len
+    }
+    let bool = count >= n ? true : false
+    return bool
+}
+
+var st = 0
+var en = 4294967295
+while st < en {
+    let mid = (st+en+1)/2
+    if kcount(mid) {
+        st = mid
+    } else {
+        en = mid - 1
+    }
+}
+
+print(st)
