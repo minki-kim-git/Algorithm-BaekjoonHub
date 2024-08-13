be09d4fde86396558bc22a9bf7ca6858f0434141
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-08-14 01:20:47 +0900

    [Silver V] Title: 중간고사 채점, Time: 20 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/15702. 중간고사 채점/중간고사 채점.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let n = io.readInt()
+let m = io.readInt()
+
+var score = [Int]()
+var maxScore = 0
+var ans = [Int]()
+
+for _ in 0..<n {
+    score.append(io.readInt())
+}
+
+for _ in 0..<m {
+    let num = io.readInt()
+    var sco = 0
+    for j in 0..<n {
+        let s = io.readString()
+        if s == "O" {
+            sco += score[j]
+        }
+    }
+    if maxScore < sco {
+        maxScore = sco
+        ans.removeAll()
+        ans.append(num)
+    } else if maxScore == sco {
+        ans.append(num)
+    }
+}
+
+print(ans.sorted().first!, maxScore)
