c22fded9cf77dfb583807e6480b316ba2cd9367c
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-08-15 23:23:12 +0900

    [level 1] Title: 최소직사각형, Time: 3.95 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -1,20 +1,20 @@
 import Foundation
 
 func solution(_ sizes:[[Int]]) -> Int {
-
-    var ga = [Int]()
-    var se = [Int]()
+    
+    var width = [Int]()
+    var length = [Int]()
+    
     for i in sizes{
         if i[0] < i[1] {
-        ga.append(i[0])
-        se.append(i[1])
+        width.append(i[0])
+        length.append(i[1])
         } else {
-             ga.append(i[1])
-             se.append(i[0])
+             width.append(i[1])
+             length.append(i[0])
         }
     }
    
-        
-    return ga.max()! * se.max()!
+    return width.max()! * length.max()!
     
 }
