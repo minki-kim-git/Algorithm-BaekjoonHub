c0ec5936599624c3adc81b79c9b5100015213119
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-08-21 18:25:49 +0900

    [Silver III] Title: 눈 치우기, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.swift
@@ -0,0 +1,86 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var arr = [Int]()
+var ans = 0
+
+for _ in 0..<n{
+    arr.append(io.readInt())
+}
+
+arr = arr.sorted(by:>)
+
+while arr.count >= 2 {
+    if arr.last! <= 0 {
+        arr.removeLast()
+        continue
+    }
+    arr[0] = arr[0] - 1
+    arr[1] = arr[1] - 1
+    ans += 1
+    arr = arr.sorted(by:>)
+}
+
+if !arr.isEmpty {
+    ans += arr.last!
+}
+
+ans > 1440 ? print(-1) : print(ans)
