2069084a13eea2db930f9a7d774447d708e28b61
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-08-29 19:12:20 +0900

    [Gold V] Title: 시간 관리, Time: 8 ms, Memory: 79648 KB -BaekjoonHub

---
+++ 백준/Gold/1263. 시간 관리/시간 관리.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var dic = [[Int:Int]]()
+
+for _ in 0..<n {
+    dic.append([io.readInt():io.readInt()])
+}
+
+func maxTime() -> Int {
+    var time = 10000000
+    for i in dic.sorted(by: { $0.first!.value > $1.first!.value}) {
+        let val = i.first!
+        if time > val.value {
+            time = val.value
+        }
+        time -= val.key
+        if time < 0 { return -1 }   
+    }
+    return time
+}
+
+print(maxTime())
