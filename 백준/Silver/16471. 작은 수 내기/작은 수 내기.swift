20fa3338f51a4ea29c2e3a971f76088e2c3b5392
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-09-24 19:12:55 +0900

    [Silver IV] Title: 작은 수 내기, Time: 44 ms, Memory: 85156 KB -BaekjoonHub

---
+++ 백준/Silver/16471. 작은 수 내기/작은 수 내기.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var ji = 0
+var si = 0
+var count = 0
+var jarr = [Int]()
+var sarr = [Int]()
+
+for _ in 0..<n {
+    jarr.append(io.readInt())
+}
+
+for _ in 0..<n {
+    sarr.append(io.readInt())
+}
+
+jarr = jarr.sorted()
+sarr = sarr.sorted()
+
+while si < n {
+    if jarr[ji] < sarr[si] {
+        count += 1
+        ji += 1
+        si += 1
+    } else {
+        si += 1
+    }
+}
+
+count >= (n+1)/2 ? print("YES") : print("NO")
