d8108342a9ab92a57a7e620fe8851e095fcf0843
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-10-08 00:09:03 +0900

    [Silver I] Title: 회전초밥, Time: 144 ms, Memory: 100564 KB -BaekjoonHub

---
+++ 백준/Silver/28107. 회전초밥/회전초밥.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let io = FileIO()
+
+let n = io.readInt()
+let m = io.readInt()
+
+var dic = [Int:[Int]]()
+var ans = Array(repeating: 0, count: n)
+var idxDIc = [Int:Int]()
+
+for i in 0..<n {
+    let k = io.readInt()
+    for _ in 0..<k {
+        let a = io.readInt()
+        if let _ = dic[a] {
+            dic[a]!.append(i)
+        } else {
+            dic[a] = [i]
+            idxDIc[a] = 0
+        }
+    }
+}
+
+for _ in 0..<m {
+    let b = io.readInt()
+    if dic[b] != nil {
+        if dic[b]!.count > idxDIc[b]! {
+            ans[dic[b]![idxDIc[b]!]] += 1
+            idxDIc[b]! = 1 + idxDIc[b]!
+        }
+    }
+}
+
+print(ans.map{String($0)}.joined(separator: " "))
