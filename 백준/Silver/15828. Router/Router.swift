5d930a9a0e458c20b80696ca0a145f2f149fce17
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-10-23 00:21:50 +0900

    [Silver IV] Title: Router, Time: 32 ms, Memory: 83620 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var idx = 0
+var arr = [Int]()
+
+func solve() -> String {
+    var ans = ""
+    if arr.count == idx { return "empty" }
+    for i in idx..<idx+n {
+        ans += "\(arr[i]) "
+        if arr.count - 1 == i { break }
+    }
+    ans.removeLast()
+    return ans
+}
+
+while true {
+    let input = io.readInt()
+    if input == -1 {
+        print(solve())
+        break
+    } else if input == 0 {
+        idx += 1
+        continue
+    }
+    if arr.count - idx < n {
+        arr.append(input)
+    }
+}
