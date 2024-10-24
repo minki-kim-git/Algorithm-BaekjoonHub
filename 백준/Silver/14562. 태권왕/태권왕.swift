6f57d1b46d439ce66d34265ee5ac6c740a078012
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-10-25 01:08:51 +0900

    [Silver I] Title: 태권왕, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/14562. 태권왕/태권왕.swift
@@ -0,0 +1,90 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let c = io.readInt()
+
+for _ in 0..<c {
+    let s = io.readInt()
+    let t = io.readInt()
+    
+    var queue = [(s,t,0)]
+    var idx = 0
+    
+    func bfs() -> Int {
+        while queue.count > idx {
+            for i in 0..<2 {
+                var sn = queue[idx].0
+                var tn = queue[idx].1
+                if i == 0 {
+                    sn *= 2
+                    tn += 3
+                } else {
+                    sn += 1
+                }
+               
+                if sn == tn { return queue[idx].2+1}
+                if  sn > tn { continue }
+                queue.append((sn,tn,queue[idx].2+1))
+                
+            }
+            idx += 1
+        }
+        return 0
+    }
+    print(bfs())
+}
