d9e15a298c33f0c4e25d0abf70dfee5ab18b5905
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-10-26 06:34:09 +0900

    [Silver IV] Title: 야근, Time: 128 ms, Memory: 82736 KB -BaekjoonHub

---
+++ 백준/Silver/14402. 야근/야근.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let q = io.readInt()
+
+var ans = 0
+var dic = [String:Int]()
+
+for _ in 0..<q {
+    let s = io.readString()
+    let p = io.readString()
+    
+    let val = dic[s] ?? 0
+    
+    if p == "+" {
+        dic[s] = val + 1
+    } else {
+        if val > 0 {
+            dic[s] = val - 1
+        } else {
+            ans += 1
+        }
+    }
+}
+
+_ = dic.map {
+    ans += $0.value
+}
+
+print(ans)
