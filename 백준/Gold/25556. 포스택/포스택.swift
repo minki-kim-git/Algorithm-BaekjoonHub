f7365005a58e813d8ff7ea77485ea5adde97c9b5
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-11-28 15:59:47 +0900

    [Gold V] Title: 포스택, Time: 20 ms, Memory: 80664 KB -BaekjoonHub

---
+++ 백준/Gold/25556. 포스택/포스택.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var ans = "YES"
+var arr = [0,0,0,0]
+
+func solve(_ num:Int) -> Bool {
+    var largest = (100001, 0)
+    for i in 0..<4 {
+        if arr[i] < num {
+            let sum = num - arr[i]
+            if largest.0 > sum {
+                largest.0 = sum
+                largest.1 = i
+            }
+        }
+    }
+    if largest.0 == 100001 { return false }
+    arr[largest.1] = num
+    return true
+}
+
+for _ in 0..<n {
+    let a = io.readInt()
+    if !solve(a) {
+        ans = "NO"
+        break
+    }
+}
+
+print(ans)
