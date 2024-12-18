bdd6162113541d575845c98e3451d1f9e1489cfa
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-12-19 01:28:31 +0900

    [Gold V] Title: 빗물, Time: 12 ms, Memory: 80700 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let h = io.readInt()
+let w = io.readInt()
+
+var ans = 0
+var arr = Array(repeating: Array(repeating: 0, count: w), count: h)
+
+for i in 0..<w {
+    let input = io.readInt()
+    for j in 0..<input {
+        arr[j][i] = 1
+    }
+}
+
+for i in arr {
+    var count = 0
+    var hei = false
+    for j in i {
+        if j == 1 && hei == false {
+            hei = true
+            count = 0
+        } else if j == 1 && hei == true {
+            ans += count
+            count = 0
+        } else if j == 0 {
+            count += 1
+        }
+    }
+}
+
+print(ans)
