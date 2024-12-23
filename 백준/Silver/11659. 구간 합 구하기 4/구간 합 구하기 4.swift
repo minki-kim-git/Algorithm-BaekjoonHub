0dda0ae4b52dac6d13a936f0192ffd4fe195fab9
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-12-23 18:41:13 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 96 ms, Memory: 82660 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.swift
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.swift
@@ -1,13 +1,74 @@
-let n = readLine()!.split(separator: " ").map{Int(String($0))!}
-let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
-var perarr = Array(repeating: 0, count: arr.count+1)
-var num = 0
-for i in 0..<arr.count{
-    num += arr[i]
-    perarr[i+1] = num
-}
-for _ in 0..<n[1]{
-    let input = readLine()!.split(separator:" ").map{Int(String($0))!}
-    print( perarr[input[1]] - perarr[input[0]-1])
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
     
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let n = io.readInt()
+let m = io.readInt()
+
+var arr = [Int]()
+var psum = Array(repeating: 0, count: n+1)
+
+for i in 1...n {
+    let input = io.readInt()
+    psum[i] = psum[i-1] + input
+}
+
+for _ in 0..<m {
+    let i = io.readInt()
+    let j = io.readInt()
+    print(psum[j] - psum[i-1])
 }
