bfba1334e7afee54fde4705c2c28b1b58f430ecd
minki-kim-git <79855248+minki-kim-git@users.noreply.github.com>
2024-12-30 18:29:19 +0900

    [Silver I] Title: 힘 겨루기, Time: 84 ms, Memory: 109964 KB -BaekjoonHub

---
+++ 백준/Silver/17251. 힘 겨루기/힘 겨루기.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let io = FileIO()
+
+let n = io.readInt()
+
+var rwin = 0
+var bwin = 0
+var maxn = 0
+var arr = [Int]()
+
+for i in 0..<n {
+    let p = io.readInt()
+    if maxn <= p {
+        if maxn < p {
+            arr.removeAll()
+            maxn = p
+        }
+        arr.append(i)
+    }
+}
+
+rwin = n - 1 - arr.last!
+bwin = arr.first!
+
+rwin < bwin ? print("B") : rwin > bwin ? print("R") : print("X")
